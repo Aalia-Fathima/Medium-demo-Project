88242c217d628196647baf891b0c9b7c06d706eb
Aalia-fathima <aaliafathima14@gmail.com>
2025-03-10 18:22:13 +0530

    production port

--- app.js
+++ app.js
@@ -56,6 +56,5 @@ app.post('/visitors', async (req, res) => {
   }
 })
 
-app.listen(8080, () => {
-  console.log("server is listening to port 8080");
-});
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
