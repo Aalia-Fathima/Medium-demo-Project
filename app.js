77ca88e0ed09ffd4b4a16289b41b3b03d6e30bde
Aalia-fathima <aaliafathima14@gmail.com>
2025-03-10 19:10:15 +0530

    change port

--- app.js
+++ app.js
@@ -56,5 +56,5 @@ app.post('/visitors', async (req, res) => {
   }
 })
 
-const PORT = process.env.PORT || 8080;
+const PORT = 80;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
